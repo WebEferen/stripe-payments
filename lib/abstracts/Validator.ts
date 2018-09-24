7698d7c23074e2feec8e5bff743e41ef1f3df74c
Mike Makowski <m.makowski@adtype.com>
2018-09-24 15:19:53 +0200

    Some documentation additions

--- lib/abstracts/Validator.ts
+++ lib/abstracts/Validator.ts
@@ -7,25 +7,44 @@ export default abstract class Validator {
   private errorMessage: string = '';
   private errorType: string = '';
 
+  /**
+   * Model verifier constructor
+   * @param modelToVerify Validation model
+   * @param verifyPattern Validation pattern
+   */
   constructor(modelToVerify: object, verifyPattern: string[] = []) {
     this.modelToVerify = modelToVerify;
     this.verifyPattern = verifyPattern;
   }
 
+  /**
+   * Set verify pattern based on the keys provided
+   * @param {string[]} pattern Pattern keys array
+   */
   public setVerifyPattern(pattern: string[]) {
     this.verifyPattern = pattern;
   }
 
+  /**
+   * Checks if verification succeed
+   */
   public isOk() {
     const modelKeys: string[] = _.keys(this.modelToVerify);
     const difference = this.checkIsOk(_.difference(this.verifyPattern, modelKeys));
     return (difference.length > 0) ? false : true;
   }
 
+  /**
+   * Gets a list of errors
+   */
   public getErrors() {
     return {type: this.errorType, message: this.errorMessage};
   }
 
+  /**
+   * Checker for the specific scenarios
+   * @param {string[]} difference Model keys to check
+   */
   private checkIsOk(difference: string[]) {
     if (difference.length > 0) {
       this.errorType = 'validation_error';
